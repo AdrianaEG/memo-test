d98b750c059a97950e9f438228b8e2ebb3a4ddd5
AdrianaEG <adriana.ngc438@gmail.com>
2020-01-20 12:01:31 -0300

    cambié cy.wait por cy.tick

--- cypress/integration/memotest.js
+++ cypress/integration/memotest.js
@@ -8,86 +8,90 @@ context('memotest', () => {
     });
 
     describe('testea el juego', () => {
-                
+
         it('se asegura que haya un tablero con cuadros', () => {
             cy.get('#comenzar').click();
             cy.get('#cartas').find('img').should('have.length', NUMERO_CARTAS);
         });
-        
+
         it('se asegura que las cartas sean aleatorias', () => {
             let clasesOriginales = [];
             cy.get('img').then((imagenes) => {
-                imagenes.each(function(i, imagen){
+                imagenes.each(function (i, imagen) {
                     clasesOriginales.push(imagen.className);
                 });
             });
-           
+
             cy.visit(URL);
-            
+
             cy.get('#comenzar').click();
             let clasesNuevas = [];
             cy.get('img').then((nuevasImagenes) => {
-                nuevasImagenes.each(function(i, imagen){
+                nuevasImagenes.each(function (i, imagen) {
                     clasesNuevas.push(imagen.className);
                 });
             });
-            
+
             cy.wrap(clasesOriginales).should('not.deep.equal', clasesNuevas);
-            
+
         });
-        
+
     });
-    
+
     describe('resuelve el juego', () => {
-       let mapaDePares, listaDePares;
+        let mapaDePares, listaDePares;
+
+        before(() => {
+            cy.clock();
+        });
         
         it('elige una combinacion erronea', () => {
-           cy.get('img').then(imagenes => {
-               mapaDePares = obtenerParesDeImagenes(imagenes);
-               listaDePares = Object.values(mapaDePares);
-               
-               console.log(listaDePares);
-               
-               cy.get(listaDePares[0][0]).click();
-               cy.get(listaDePares[1][0]).click();
-               
-               cy.get('img').should('have.length', NUMERO_CARTAS);
-           }); 
+            cy.get('img').then(imagenes => {
+                mapaDePares = obtenerParesDeImagenes(imagenes);
+                listaDePares = Object.values(mapaDePares);
+
+                console.log(listaDePares);
+
+                cy.get(listaDePares[0][0]).click();
+                cy.get(listaDePares[1][0]).click();
+
+                cy.get('img').should('have.length', NUMERO_CARTAS);
+            });
         });
-        
+
         it('resuelve el juego', () => {
-           cy.get('img').should('have.length', NUMERO_CARTAS);
-            
+
+            cy.get('img').should('have.length', NUMERO_CARTAS);
+
             listaDePares.forEach((par) => {
-                cy.wait(1000);
+                cy.tick(1000);
                 cy.get(par[0]).click();
                 cy.get(par[1]).click();
             });
-            
+
             cy.get('img').should('have.length', 0);
-                        
+
             cy.get('#estado').contains('GANASTE!!! Hace click en comenzar para empezar de nuevo :)');
-            
+
         });
-        
+
     });
 
 });
 
-function obtenerParesDeImagenes(imagenes){
+function obtenerParesDeImagenes(imagenes) {
     const pares = {};
-    
-    imagenes.each((i, imagen) =>{
+
+    imagenes.each((i, imagen) => {
         const claseImagen = imagen.className.replace('img-fluid fondo-imagenes ', '');
-        
-        if(pares[claseImagen]){
+
+        if (pares[claseImagen]) {
             pares[claseImagen].push(imagen);
-        }
-        else{
+        } else {
             pares[claseImagen] = [imagen];
         }
     });
-    
+
     //console.log(pares);
     return pares;
 }
